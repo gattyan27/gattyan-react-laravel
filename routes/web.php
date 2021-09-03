66de39724cb5f052c1eb9d5a77173dfe5540ede3
gattyan27 <radio.radirer@gmail.com>
2021-09-04 02:05:52 +0900

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 # どのルートにもマッチしないときに
 Route::fallback(function () {
     # public/index.htmlにはCRAのビルド結果がおいてある
